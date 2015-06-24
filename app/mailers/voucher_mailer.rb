00152d62bb54429669d2bc9e48f9e4e787270756
David Ladowitz <david@ladowitz.com>
2015-06-24 11:25:43 -0700

    rake task

--- app/mailers/voucher_mailer.rb
+++ app/mailers/voucher_mailer.rb
@@ -1,11 +1,12 @@
 class VoucherMailer < ApplicationMailer
   # layout "user_mailer"  # css is not showing up properly when using a layout
 
-    def voucher_demo_email(voucher)
+    def voucher_email(voucher)
       @voucher = voucher
       @location = voucher.location
       @is_birthday = @voucher.customer.birthday.month == Date.today.month
 
       mail(to: @voucher.customer.email, subject: "Monthly Deal from #{@location.name}")
+      @voucher.update(sent: true)
     end
   end
---
+++ lib/tasks/vouchers.rake
@@ -0,0 +1,13 @@
+# namespace :vouchers do
+#   # desc "Send vouchers for recent location signups"
+#   task :send_recent do
+#     puts "I'm sending all the newly created vouchers"
+#   end
+# end
+
+namespace :vouchers do
+  desc "Send vouchers for recent location signups"
+  task :send_recent do
+    puts "I'm sending all the newly created vouchers"
+  end
+end
